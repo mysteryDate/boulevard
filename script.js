2f5fc16f9ed27c187fdd5adb71cac0c887352d91
Aaron Krajeski <aaronkrajeski@gmail.com>
2013-09-18 09:53:04 -0400

    Somehow got the linear scrolling working better

--- script.js
+++ script.js
@@ -9,42 +9,41 @@ var viewState = sectionTitles[0];
 var backgroundImage = new Image();
 backgroundImage.src = "background.jpg"; 
 
+var totalHeight = 1.2 * $(window).width(); 	// Const should be the apsect ratio of the background
+var divisionDistance = totalHeight / numSections;		// Should be total height over number of sections, I need to fiture out what total height is
+var windowHeight = $(window).height();		// Just so it only runs once
+
 var sectionHeight;
 var sections = [];
 
 // Each frame on the page
-function section(name) {
+function section(name, number) {
 
+	var self = this;
 	this.$self;			// A jquery object for the section itself
 	this.name = name;
 
-	this.postion;		// The y value (in pixels) of the top of the frame when centered
+	this.mark = divisionDistance * number;		// The y value (in pixels) of the top of the frame when centered
+	this.yPos;
 
-	this.center = function() {
-		var yPosition = window.scrollY;
-		this.$self.css('top', yPosition);
+	this.position = function() {
+		this.$self.css('top', self.yPos);
 	}
 
 }
 
 $(document).ready(function() {
 
-	console.log(backgroundImage.clientHeight);
-
-
 	// Create all of the sections
 	for (var i = 0; i < numSections; i++) {
 		var title = sectionTitles[i]
-		sections[i] = new section(title);
+		sections[i] = new section(title, i);
 		$('body').append('<div class="sections" id='+title+'>');
 		sections[i].$self = $('#'+title);
 	};
 
 	backgroundImage.onload = function () {
 		$('body').css('background-image', 'url("background.jpg")');
-
-		console.log(backgroundImage.clientHeight);
-		var sections = []
 
 		for (var i = 0; i < numSections; i++) {
 			
@@ -54,7 +53,6 @@ $(document).ready(function() {
 		$('.sections').each(function(i, element) {
 			$(this).css('top', $(window).height()*i);
 			$(this).append("<h1>"+sectionTitles[i]+"</h1>");
-			console.log(this, i, element);
 		});
 
 		$('body').append(
@@ -67,11 +65,16 @@ $(document).ready(function() {
 });
 
 window.onresize = function (e) {
-	$('.sections').each(function(index, element) {
-		$(this).css({
-			'top': $(window).height()*index
-		});
-	});
+
+	var totalHeight = 1.2 * $(window).width(); 	// Const should be the apsect ratio of the background
+	var divisionDistance = totalHeight / numSections;		// Should be total height over number of sections, I need to fiture out what total height is
+	var windowHeight = $(window).height();		// Just so it only runs once
+
+	for (var i = 0; i < sections.length; i++) {
+		sections[i].mark = divisionDistance*i;
+		sections[i].position();
+	};
+	// change total height and div distance and marks
 }
 
 window.onscroll = function(e) {
@@ -80,17 +83,28 @@ window.onscroll = function(e) {
 
 function position_panels() {
 
-	var totalHeight = 1.2 * $(window).width(); 	// 1.5 should be the apsect ratio of the background
-	var divisionDistance = totalHeight / numSections;		// Should be total height over number of sections, I need to fiture out what total height is
-	var windowHeight = $(window).height();		// Just so it only runs once
+	// top = -(scrollY + window.width)(scrollY - D^2*num)^-5(scrollY - D*num)^5
+	// D = total height / number of frames
+	// num = the numeric position of that frame (starting at 0)
+
 	var scrollY = window.scrollY;
 
 	for (var i = 0; i < sections.length; i++) {
-		var b = i * windowHeight;
-		var m = windowHeight / divisionDistance;
-		var y = m * scrollY - b;
-		if (i == 2) {console.log(sections[2].name, y, y-window.scrollY, window.scrollY);};
-		sections[i].$self.css('top', (scrollY - y)+'px');
+
+		var $frame = sections[i].$self;
+		var mark = sections[i].mark;
+		var disp = mark - scrollY;		// Displacement of the current frame from view
+
+		// To avoid a quintic calulation for sections out of the frame
+		if ( disp > windowHeight || disp < -windowHeight ) $frame.css('display', 'none');		
+		
+		else {
+			$frame.css('display', 'block');
+			var b = i * windowHeight;
+			var m = windowHeight / divisionDistance;
+			var y = m * scrollY - b;
+			sections[i].$self.css('top', (scrollY - y)+'px');
+		}
 	};
 
 	
